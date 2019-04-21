3e66a512682feac4f58a634f92c747cd71827367
Nahuel Aquino <nahuel89.aquino@gmail.com>
2019-04-21 20:04:52 -0300

    primaras practicas con Java

---
+++ src/Bienvenido2.java
@@ -0,0 +1,14 @@
+// impresion de una linea de texto con varias instrucciones e impresion de varias lienas con una sola instruccion
+// impresion de una linea de texto con formato
+public class Bienvenido2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Bienvenido a la "); // no posiciona el cursor al inicio de la siguiente linea
+		System.out.println("programacion en Java");
+		System.out.println("Bienvenido a la \n programacion en \n\n Java");
+		System.out.printf("\n\n%s\n%s", "Bienvenido a la", "programacion en Java!!!");
+
+	}
+
+}
---
+++ src/HolaMundo.java
@@ -0,0 +1,10 @@
+// Programa para imprimir texto
+
+public class HolaMundo {
+	// el metodo main comienza las aplicaciones en Java
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenido a la programacion en Java");
+	}// fin del metodo main
+
+}// Fin de la clase hola mundo

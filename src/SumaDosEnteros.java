b45333e1630af33179d71a4bb7593fc7c164fe87
Nahuel Aquino <nahuel89.aquino@gmail.com>
2019-04-21 22:02:04 -0300

    modificacion de archivo SumaDosEnteros

--- src/SumaDosEnteros.java
+++ src/SumaDosEnteros.java
@@ -1,9 +1,26 @@
+// programa que suma dos enteros
+import java.util.Scanner; //importacion de la clase Scanner
 
 public class SumaDosEnteros {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// crea el objeto Scanner para obtener la entrada de la ventana de comandos
+		Scanner entrada = new Scanner(System.in); 
+		
+		int num1; //primer numero a sumar
+		int num2; // segundo numero a sumar
+		int suma; // suma de num1 y num2
+		System.out.println("Escriba el primer numero a sumar: ");
+		num1 = entrada.nextInt(); // lee el primer numero del usuario
+		
+		System.out.println("Escriba el segundo numero a sumar: ");
+		num2 = entrada.nextInt(); // lee el segundo numero del usuario
+		
+		suma = num1 + num2; // suma los numero enteros y los almacena en suma
+		
+		System.out.printf("La suma de %d y %d es %d", num1, num2, suma); // muestra la suma
+		
+		entrada.close();
+	}// fin del metodo main
 
-	}
-
-}
+}// fin de la clase SumaDeEnteros
